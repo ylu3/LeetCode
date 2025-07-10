5c2938ac580f41251b2988ae70b7cf1efc596b43
ylu3 <luyansasc@gmail.com>
2025-07-10 14:52:24 +0200

    update 139

--- DP/139.py
+++ DP/139.py
@@ -3,38 +3,50 @@ from typing import List
 
 
 class Solution:
+    # def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+    #     d = set(wordDict)
+    #     @cache
+    #     def wordBreak_internal(s: str):
+    #         if not s:
+    #             return True
+    #         for i in range(len(s)):
+    #             if s[:i+1] in d and wordBreak_internal(s[i+1:]):
+    #                 return True
+    #         return False
+    #
+    #     return wordBreak_internal(s)
+
+    # 根据最大单词长度剪枝
+    # def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+    #     d = set(wordDict)
+    #     k = max(len(w) for w in wordDict)
+    #     @cache
+    #     def wordBreak_internal(s: str):
+    #         if not s:
+    #             return True
+    #         for i in range(len(s)):
+    #             if i + 1 > k:
+    #                 break
+    #             if s[:i+1] in d and wordBreak_internal(s[i+1:]):
+    #                 return True
+    #         return False
+    #
+    #     return wordBreak_internal(s)
+
+    # 迭代写法
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         d = set(wordDict)
-        @cache
-        def wordBreak_internal(s: str):
-            if not s:
-                return True
-            for i in range(len(s)):
-                if s[:i+1] in d and wordBreak_internal(s[i+1:]):
-                    return True
-            return False
-
-        return wordBreak_internal(s)
-
-        # 根据最大单词长度剪枝
-        # def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        #     d = set(wordDict)
-        #     k = max(len(w) for w in wordDict)
-        #     @cache
-        #     def wordBreak_internal(s: str):
-        #         if not s:
-        #             return True
-        #         for i in range(len(s)):
-        #             if i + 1 > k:
-        #                 break
-        #             if s[:i+1] in d and wordBreak_internal(s[i+1:]):
-        #                 return True
-        #         return False
-        #
-        #     return wordBreak_internal(s)
+        dp = [False] * len(s)
+        dp[0] = s[0] in d
+        for i in range(1, len(s)):
+            for j in range(0, i + 1):
+                if s[j:i + 1] in d and (j - 1 < 0 or dp[j - 1]):
+                    dp[i] = True
+        print(dp)
+        return dp[-1]
 
 
-print(Solution().wordBreak("applepenapple", ["apple","pen"]))
+print(Solution().wordBreak("ab", ["a", "b"]))
 
 # 没有缓存的情况下，本质上是生成字符串所有的分割方式（数量不限），每一个位置可以选择割与不割，因此复杂度为2**n
 # 如果缓存那么在最外层循环的第一次（i=0）结束后就会覆盖所有可能的wordBreak_internal(i)，因此递归最外层i>1之后都是O(1)
